62e0e91b00743f2723f740b7a820e31e068b55a7
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-01-06 14:09:29 -0800

    twilio sms test

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var client=require('twilio')(
+	process.env.TWILIO_ACCOUNT_SID,
+	process.env.TWILIO_AUTH_TOKEN
+);
+
+client.messages.create({
+	from: process.env.TWILIO_PHONE_NUMBER,
+	to: process.env.CELL_PHONE_NUMBER,
+	body: "evac has started!  you have twenty minutes until reset."
+	
+}).then((message) => console.log(message.sid));
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
