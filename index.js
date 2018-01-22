2a0cb55a5b023a5a7e1e554f063b422d140c4d26
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-01-22 12:11:59 -0800

    adding test for url builder

--- index.js
+++ index.js
@@ -34,14 +34,30 @@ function textforspeechURL(textforspeech){
 	return process.env.VENT_URL+"getVoiceTwiml?textforspeech="+encodeURIComponent(textforspeech);
 }		
 
-		
+
+app.get('/testBuildGetUrl',(req,res)=>{
+	paramArray={
+		'name':'shuang van reizen',
+		'specialty':'vr'
+	};
+	console.log(buildGetUrl(process.env.VENT_URL,paramArray));
+});
+function buildGetUrl(baseUrl,paramArray){
+	url=baseUrl+"?";
+	Object.keys(paramArray).forEach(function(key){
+		url+=key+"="+encodeURIComponent(paramArray[key])+"&";
+	});
+	url=url.slice(0,-1);
+	return url;
+	
+}		
 
 app.post('/voice',(req,res)=>{
 	console.log("reached voice endpoint");
 	url=process.env.VENT_URL+"callHost";
 	console.log("SID for this call is: "+req.body.CallSid);
 	
-	/*
+	
 	const response=new VoiceResponse();
 	
 	var call=client.calls.create({
@@ -50,7 +66,7 @@ app.post('/voice',(req,res)=>{
 		from: process.env.TWILIO_PHONE_NUMBER,
 		method: 'GET'
 	});
-	*/
+	
 		
 });
 
