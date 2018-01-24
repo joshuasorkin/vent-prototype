5fca9dc99ba5b60242bb6ae00fcee63d3b746070
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-01-23 20:05:18 -0800

    created dbtest for testing postgres operations

---
+++ dbtest.js
@@ -0,0 +1,17 @@
+const Client=require('pg');
+
+const client=new Client({
+	connectionString:process.env.DATABASE_URL,
+	ssl:true
+});
+
+client.connect();
+client.query("insert into users phonenumber values ('+15105753138');");
+
+client.query('SELECT phonenumber FROM users;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
