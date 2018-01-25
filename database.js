b9469f751da1208a3d34560ac66602f2671be9f4
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-01-24 18:51:46 -0800

    trying alternate module.exports declaration

--- database.js
+++ database.js
@@ -8,28 +8,29 @@ const pgclient=new Client({
 
 pgclient.connect();
 	
-module.exports = {
-	getUser:function(phonenumber, callback){
-        queryStr='SELECT * FROM users where phonenumber=\''+phonenumber+'\';';
-        console.log("queryStr: "+queryStr);  
-		abc.run().then(
-			console.log("running abc");
-		).catch(err=>console.error(err.stack);
-		pgclient.query(queryStr)
-			.then(res => {
-				console.log("query running");
-				console.log("rows length "+res.rows.length);
-				if (res.rows.length==0){
-					return null;
-				}
-				else{
-					return res.rows[0];
-				}
-			})
-			.catch(err=>console.error(err.stack));
-	},			
+function getUser(phonenumber, callback){
+	queryStr='SELECT * FROM users where phonenumber=\''+phonenumber+'\';';
+	console.log("queryStr: "+queryStr);  
+	abc.run().then(
+		console.log("running abc");
+	).catch(err=>console.error(err.stack);
+	pgclient.query(queryStr)
+		.then(res => {
+			console.log("query running");
+			console.log("rows length "+res.rows.length);
+			if (res.rows.length==0){
+				return null;
+			}
+			else{
+				return res.rows[0];
+			}
+		})
+		.catch(err=>console.error(err.stack));
+}			
 
-	destruct:function(){
-		pgclient.end();
-	}
+function destruct(){
+	pgclient.end();
 }
+
+module.exports.getUser=getUser;
+module.exports.destruct=destruct;
