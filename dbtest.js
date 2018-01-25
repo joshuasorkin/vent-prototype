4969cc2a18f06f9774f66e11136ae634f611026b
joshua sorkin <joshua.d.sorkin@gmail.com>
2018-01-24 19:07:28 -0800

    trying it with the code in dbtest instead of another module

--- dbtest.js
+++ dbtest.js
@@ -1,3 +1,46 @@
+const {Client}=require('pg');
+
+const pgclient=new Client({
+	connectionString:process.env.DATABASE_URL,
+	ssl:true
+});
+
+pgclient.connect();
+	
+function getUser(phonenumber, callback){
+	queryStr='SELECT * FROM users where phonenumber=\''+phonenumber+'\';';
+	console.log("queryStr: "+queryStr);  
+	pgclient.query(queryStr)
+		.then(res => {
+			console.log("query running");
+			console.log("rows length "+res.rows.length);
+			if (res.rows.length==0){
+				return null;
+			}
+			else{
+				return res.rows[0];
+			}
+		})
+		.catch(err=>console.error(err.stack));
+}			
+
+getUser('+19991112222', function(err, user) {
+    if (err) throw err;
+    if (user==null){
+        console.log("user not found");
+    }
+    else {
+        console.log(user["phonenumber"]);
+    }
+});
+
+
+
+
+
+
+
+
 var db=require('./database');
 
 db.getUser('+19991112222', function(err, user) {
